f7594014e7d2dbbe9b532b2c4e517f7f0936aa34
Vass Máté <h881187@stud.u-szeged.hu>
2021-05-06 02:12:06 +0200

    created default project, initialized Firebase

---
+++ FHIR_Medication/app/src/main/java/com/example/fhir_medication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fhir_medication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
