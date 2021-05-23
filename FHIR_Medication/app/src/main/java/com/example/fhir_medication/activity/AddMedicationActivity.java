5c5f7c856c8545c7407e3f869295b5ab44136d0d
Vass Máté <h881187@stud.u-szeged.hu>
2021-05-23 02:05:16 +0200

    medication add works

--- FHIR_Medication/app/src/main/java/com/example/fhir_medication/activity/AddMedicationActivity.java
+++ FHIR_Medication/app/src/main/java/com/example/fhir_medication/activity/AddMedicationActivity.java
@@ -15,10 +15,11 @@ import com.example.fhir_medication.model.MedicationModel;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.util.Date;
 
 public class AddMedicationActivity extends AppCompatActivity {
 
@@ -27,23 +28,21 @@ public class AddMedicationActivity extends AppCompatActivity {
 
     Button backButton;
     Button addButton;
-    EditText identifier1;
-    EditText identifier2;
-    EditText code;
-    EditText status;
-    EditText manufacturer;
-    EditText form;
-    EditText amount;
-    EditText ingredient1Item;
-    RadioGroup ingredient1IsActive;
-    EditText ingredient1Strength;
-    EditText ingredient2Item;
-    RadioGroup ingredient2IsActive;
-    EditText ingredient2Strength;
-    EditText batch1LotNumber;
-    EditText batch1ExpirationDate;
-    EditText batch2LotNumber;
-    EditText batch2ExpirationDate;
+    EditText identifier1_IN;
+    EditText identifier2_IN;
+    EditText code_IN;
+    EditText status_IN;
+    EditText manufacturer_IN;
+    EditText form_IN;
+    EditText amount_IN;
+    EditText ingredient1Item_IN;
+    RadioGroup ingredient1IsActive_IN;
+    EditText ingredient1Strength_IN;
+    EditText ingredient2Item_IN;
+    RadioGroup ingredient2IsActive_IN;
+    EditText ingredient2Strength_IN;
+    EditText batchLotNumber_IN;
+    EditText batchExpirationDate_IN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,22 +55,77 @@ public class AddMedicationActivity extends AppCompatActivity {
 
         backButton = findViewById(R.id.backButton);
         addButton = findViewById(R.id.addButton);
+        identifier1_IN = findViewById(R.id.identifier1);
+        identifier2_IN = findViewById(R.id.identifier2);
+        code_IN = findViewById(R.id.code);
+        status_IN = findViewById(R.id.status);
+        manufacturer_IN = findViewById(R.id.manufacturer);
+        form_IN = findViewById(R.id.form);
+        amount_IN = findViewById(R.id.amount);
+        ingredient1Item_IN = findViewById(R.id.ingredient1_item);
+        ingredient1IsActive_IN = findViewById(R.id.ingredient1_isActive);
+        ingredient1Strength_IN = findViewById(R.id.ingredient1_strength);
+        ingredient2Item_IN = findViewById(R.id.ingredient2_item);
+        ingredient2IsActive_IN = findViewById(R.id.ingredient2_isActive);
+        ingredient2Strength_IN = findViewById(R.id.ingredient2_strength);
+        batchLotNumber_IN = findViewById(R.id.batch_lotNumber);
+        batchExpirationDate_IN = findViewById(R.id.batch_expirationDate);
 
         backButton.setOnClickListener(v -> {
             finish();
         });
 
         addButton.setOnClickListener(v -> {
-            MedicationModel item11 = new MedicationModel(new ArrayList<>(
-                    Arrays.asList("med0311", "medication0311")), "asd",
-                    null, "asd", "asd",
-                    null,
+            String identifier1 = identifier1_IN.getText().toString();
+            String identifier2 = identifier2_IN.getText().toString();
+            String code = code_IN.getText().toString();
+            String status = status_IN.getText().toString();
+            String manufacturer = manufacturer_IN.getText().toString();
+            String form = form_IN.getText().toString();
+            Integer amount = null;
+            if (!amount_IN.getText().toString().equals("")) {
+                amount = Integer.parseInt(amount_IN.getText().toString());
+            }
+            String ingredient1Item = ingredient1Item_IN.getText().toString();
+            Boolean ingredient1IsActive = null;
+            switch (ingredient1IsActive_IN.getCheckedRadioButtonId()) {
+                case 0: ingredient1IsActive = true; break;
+                case 1: ingredient1IsActive = false; break;
+            }
+            Integer ingredient1Strength = null;
+            if (!ingredient1Strength_IN.getText().toString().equals("")) {
+                Integer.parseInt(ingredient1Strength_IN.getText().toString());
+            }
+            String ingredient2Item = ingredient2Item_IN.getText().toString();
+            Boolean ingredient2IsActive = null;
+            switch (ingredient2IsActive_IN.getCheckedRadioButtonId()) {
+                case 0: ingredient2IsActive = true; break;
+                case 1: ingredient2IsActive = false; break;
+            }
+            Integer ingredient2Strength = null;
+            if (!ingredient2Strength_IN.getText().toString().equals("")) {
+                Integer.parseInt(ingredient2Strength_IN.getText().toString());
+            }
+            String batchLotNumber = batchLotNumber_IN.getText().toString();
+            Date batchExpirationDate = null;
+            if (!batchExpirationDate_IN.getText().toString().equals("")) {
+                try {
+                    batchExpirationDate = new SimpleDateFormat("dd/MM/yyyy")
+                            .parse(batchExpirationDate_IN.getText().toString());
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+            Integer imageResource = null;
+
+            MedicationModel item = new MedicationModel(new ArrayList<>(
+                    Arrays.asList(identifier1, identifier2)), code, status, manufacturer, form, amount,
                     new ArrayList<>(Arrays.asList(
-                            new IngredientModel("asd", null, 500))),
-                    new BatchModel("123",
-                            new GregorianCalendar(2017, Calendar.MAY, 22).getTime()),
-                    null);
-            mItems.add(item11);
+                            new IngredientModel(ingredient1Item, ingredient1IsActive, ingredient1Strength),
+                            new IngredientModel(ingredient2Item, ingredient2IsActive, ingredient2Strength))),
+                    new BatchModel(batchLotNumber, batchExpirationDate), imageResource);
+
+            mItems.add(item);
             Toast.makeText(AddMedicationActivity.this,
                     "Item added successfully", Toast.LENGTH_LONG).show();
             finish();
